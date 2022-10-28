429dceb72b18c2ee81a6318fe150584f087df8fa
ckieverentineas <ckieverentineas@yandex.ru>
2022-10-28 22:10:52 +1000

    added items

--- prisma/migrations/20220816034518_add_artefact/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Artefact" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "id_user" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    CONSTRAINT "Artefact_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
