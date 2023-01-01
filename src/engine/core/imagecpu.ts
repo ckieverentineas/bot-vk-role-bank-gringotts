e526d9f0bc3579a9666117f9f8f88410d49e8fa6
ckieverentineas <ckieverentineas@yandex.ru>
2023-01-01 19:24:52 +1000

    add on card idvk for number card

--- src/engine/core/imagecpu.ts
+++ src/engine/core/imagecpu.ts
@@ -6,12 +6,11 @@ import { promises as fs } from 'fs';
 
 export async function Image_Text_Add_Card(context: any, x: number, y: number, text: any) {
     const dir = `./src/art/template/card`
-    //const file_name: any = await readDir(dir)
-    //const lenna = await Jimp.read(`${dir}/${file_name[randomInt(0, file_name.length)]}`)
-    const lenna = await Jimp.read(`${dir}/card_cristmas.jpg`)
+    const file_name: any = await readDir(dir)
+    const lenna = await Jimp.read(`${dir}/${file_name[randomInt(0, file_name.length)]}`)
     const font = await Jimp.loadFont('./src/art/font/impact_medium/impact.fnt')
-    const font_big = await Jimp.loadFont('./src/art/font/impact_big/impact.fnt')
-    const res = await lenna.resize(1687, 1077).print(font_big, x, y, ('0000000000000000'+text.id).slice(-16).replace(/\d{4}(?=.)/g, '$& ').replace(/ /g, `${' '.repeat(7)}`))
+    const font_big = await Jimp.loadFont('./src/art/font/impact_big/impact.fnt') 
+    const res = await lenna.resize(1687, 1077).print(font_big, x, y, (`${text.idvk * Math.pow(10, 16-String(text.idvk).length)+text.id}`).slice(-16).replace(/\d{4}(?=.)/g, '$& ').replace(/ /g, `${' '.repeat(7)}`))
     .print(font, x, y+200, text.name, 1200)
     .print(font, lenna.getWidth()-370, y+200, text.crdate.toLocaleDateString('de-DE', { year: "numeric", month: "2-digit", day: "2-digit" }) )
     
